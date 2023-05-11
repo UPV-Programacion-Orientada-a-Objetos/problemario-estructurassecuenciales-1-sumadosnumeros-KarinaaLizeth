d71d15b28cbd6f1093dbad4be691ac27e05f9b6e
Karina <2130045@upv.edu.mx>
2023-05-11 11:40:49 -0600

    Listo

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 class Main {
-
     public static void main(String[] args) {
 
+        int a = Integer.parseInt(args[0]); //args datos que provienen de la línea de comandos
+        int b = Integer.parseInt(args[1]); //parseInt cambia de string a integer
+
+        SumaDosNumeros obj = new SumaDosNumeros();
+        
+        System.out.println(obj.suma(a,b));
     }
 }
